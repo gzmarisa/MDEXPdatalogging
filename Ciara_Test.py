ab6f070a82e9117ab69ffadefea70e461654a6ea
Ciara Avelina Lugo <ciara@dhcp-10-142-207-79.uawifi.arizona.edu>
2019-05-15 16:41:57 -0700

    wooo

--- Ciara_Test.py
+++ Ciara_Test.py
@@ -1,4 +1,6 @@
 import sys
+import time as t
+import datetime
 from CProbe import ConductivityProbe
 from TProbes import TemperatureProbes
 from Scale import Scale
@@ -7,32 +9,76 @@ def fprintf(stream, format_spec, *args):
     stream.write(format_spec % args)
 
 
-con = ConductivityProbe(9600)
-con.whosmans()
-con.helpme()
-con.changeB(80000)
-con.changeP()
-con.whosmans()
-#con.openC()
-#l = con.line()
-#print(l)
+def main():
+    con = ConductivityProbe(2, 9600)
+    con.whosmans()
+    con.helpme()
+    con.changeB(80000)
+    con.changeP(1)
+    con.whosmans()
+    #con.openC()
+    #l = con.line()
+    #print(l)
 
-T = TemperatureProbes(8909890)
-T.whosmans()
-T.helpme()
-T.changeB(80000)
-T.changeP()
-T.whosmans()
-#T.openC()
-#l = T.line()
-#print(l)
+    T = TemperatureProbes(1, 8909890)
+    T.whosmans()
+    T.helpme()
+    T.changeB(80000)
+    T.changeP(0)
+    T.whosmans()
+    #T.openC()
+    #l = T.line()
+    #print(l)
 
-S = Scale(8909890)
-S.whosmans()
-S.helpme()
-S.changeB(80000)
-S.changeP()
-S.whosmans()
-#S.openC()
-#l = S.line()
-#print(l)
+    S = Scale(0, 8909890)
+    S.whosmans()
+    S.helpme()
+    S.changeB(80000)
+    S.changeP(7)
+    S.whosmans()
+    #S.openC()
+    #l = S.line()
+    #print(l)
+
+    #loop for making the code start when seconds = 0
+    time = t.localtime(None).tm_sec
+    while(time != 0):
+        time = t.localtime(None).tm_sec
+        #print("can you wait?")
+        
+    print(t.localtime(None))
+    print("Month","Day","Hour","Minute","Second",sep='\t')
+
+main()
+month = []
+day = []
+hour = []
+minute = []
+second = []
+i = 0
+while (True):
+    #print("I'm alive, I swear. Just don't ask me to complete a CAPTCHA")
+    d = datetime.date.today()
+    c = datetime.datetime.now()
+    #year = d.year
+    month.append(d.month)
+    day.append(d.day)
+    hour.append(c.hour)
+    minute.append(c.minute)
+    second.append(c.second)
+    #print(second)
+    m = second[i] % 30
+    f = 0
+    #if (m == 0):
+        #f = f+1
+        #print (f)
+        #print(m)
+    #if (f == 1) & (m==0):
+    if (m==0):
+        print(month[i], day[i], hour[i], minute[i], second[i], sep='\t')
+    t.sleep(1)
+    i = i +1 
+    #print("Still kickin")
+
+
+
--- TProbes.py
+++ TProbes.py
@@ -1,28 +1,47 @@
-import serial
+import pyserial
 
 class TemperatureProbes:
-
-    def __init__(self, baud):
-        self.port = str(input('What is the port name?   '))
+    def __init__(self, n, baud):
         self.baud = baud
-
-#Prints Port name and Baud rate, in case you forgot
+        if (n==0):
+            self.port = '/dev/ttyACM0'
+        elif (n==1):
+            self.port = '/dev/ttyACM1'
+        elif (n==2):
+            self.port = '/dev/ttyACM2'
+        elif (n==3):
+            self.port = '/dev/ttyACM3'
+        else:
+            print("Invalid ending")
+            exit
+        
+     #Prints Port name and Baud rate, in case you forgot
     def whosmans(self):
         print("Port name is " + self.port)
-        print("\n Baud Rate is " + str(self.baud))
+        print("Baud Rate is " + str(self.baud))
 
     #Prints method names and description
     def helpme(self):
-        methods = ["whosmans", "changeP ", "changeB ", "openC   "]
-        inputs = ["N/A              ", "N/A              ", "New Baud Rate (int)", "N/A              "]
-        desc = ["Output port name and baud rate.", "Change port name", "Change baud rate", "Open serial port for probe"]
+        methods = ["whosmans", "changeP ", "changeB ", "openC   ", "line    "]
+        inputs = ["N/A              ", "N/A              ", "New Baud Rate (int)", "N/A              ", "N/A              ",]
+        desc = ["Output port name and baud rate.", "Change port name", "Change baud rate", "Open serial port for probe", "Gets serial output data"]
         
         for i in range(4):
             print(methods[i] + '    ' + inputs[i] + '    ' + desc[i])
         
     #Change port name
-    def changeP(self):
-        self.port = str(input('What is the port name?   '))
+    def changeP(self, n):
+        if (n==0):
+            self.port = '/dev/ttyACM0'
+        elif (n==1):
+            self.port = '/dev/ttyACM1'
+        elif (n==2):
+            self.port = '/dev/ttyACM2'
+        elif (n==3):
+            self.port = '/dev/ttyACM3'
+        else:
+            print("Invalid ending")
+            exit
 
     #Change Baud Rate 
     def changeB(self, Nbaud):
